822f300e00246210324648d06e24900a3e76e3c2
nizarmust19-ux <nizarmust19@gmail.com>
2026-09-06 23:15:48 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
-// --- MODUL OTENTIKASI / LOGIN ADMIN ---
+// --- MODUL OTENTIKASI KEAMANAN INPUT ---
 let isLogedIn = false;
 
-function bukaAdmin() {
+function bukaFormInput() {
     if (isLogedIn) {
         let secAdmin = document.getElementById('section-admin');
         secAdmin.classList.remove('hidden');
@@ -9,7 +9,7 @@ function bukaAdmin() {
         return;
     }
 
-    let pass = prompt("Masukkan Password Pengurus RT:");
+    let pass = prompt("Masukkan Password Khusus Pengurus:");
     if (pass === CONFIG.passwordAdmin) {
         isLogedIn = true;
         let secAdmin = document.getElementById('section-admin');
@@ -20,6 +20,6 @@ function bukaAdmin() {
     }
 }
 
-function tutupAdmin() {
+function tutupFormInput() {
     document.getElementById('section-admin').classList.add('hidden');
 }
