278747479475c6088274a077e671734ab2017db7
nizarmust19-ux <nizarmust19@gmail.com>
2026-09-07 11:40:04 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// --- KONFIGURASI LINK & KREDENSIAL ---
-const CONFIG = {
-    csvKasUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTYIf50Z442h-scILSksvfBuyipCJYBEqMPAM4ZX0tidMNJ7ksu7WTBVTTY7yvEj8fBhlkzJ8N0js3i/pub?output=csv',
-    csvPengurusUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTYIf50Z442h-scILSksvfBuyipCJYBEqMPAM4ZX0tidMNJ7ksu7WTBVTTY7yvEj8fBhlkzJ8N0js3i/pub?gid=163682397&single=true&output=csv',
-    sheetDbApiUrl: 'https://sheetdb.io/api/v1/rn6pg1mlyylzh',
-    apiSaranUrl: 'https://sheetdb.io/api/v1/w47ybuoc08480',
-    passwordAdmin: 'rt0103'
-};
