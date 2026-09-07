ad9c7be19f5c38a22f0f69b2d6cdfd6099f662e0
nizarmust19-ux <nizarmust19@gmail.com>
2026-09-07 11:55:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // --- KONFIGURASI LINK & KREDENSIAL ---
-const CONFIG = {
+export const CONFIG = {
     csvKasUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTYIf50Z442h-scILSksvfBuyipCJYBEqMPAM4ZX0tidMNJ7ksu7WTBVTTY7yvEj8fBhlkzJ8N0js3i/pub?output=csv',
     csvPengurusUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTYIf50Z442h-scILSksvfBuyipCJYBEqMPAM4ZX0tidMNJ7ksu7WTBVTTY7yvEj8fBhlkzJ8N0js3i/pub?gid=163682397&single=true&output=csv',
     sheetDbApiUrl: 'https://sheetdb.io/api/v1/rn6pg1mlyylzh',
